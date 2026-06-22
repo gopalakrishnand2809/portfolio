ff4ad7d1be201a14e9532332a9059120da3aa8ad
Gopalakrishnan Devanathan <gopalakrishnand28@gmail.com>
2026-06-22 10:41:35 +0530

    portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ink: '#0a0a0f',
+        paper: '#f5f3ef',
+        acid: '#c8ff00',
+        acid2: '#ff3cac',
+        steel: '#6b7280',
+      },
+      fontFamily: {
+        syne: ['Syne', 'sans-serif'],
+        mono: ['DM Mono', 'monospace'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
